556e919649e40d6db073cf0721545ca3c6c3be96
nathanielnosa <52939265+nathanielnosa@users.noreply.github.com>
2020-08-23 14:07:03 +0100

    show password commit

---
+++ psd.js
@@ -0,0 +1,10 @@
+const pwd = document.querySelector('#pwd');
+const seepwd = document.querySelector('#see');
+
+seepwd.addEventListener('click',()=>{
+
+  (pwd.type !=='password')?
+  document.body.innerHTML = "Enter a text"
+  :console.log(pwd.type =Text);
+
+});
